97a6a6d4d9ff5e861e9aa07dfa2087fb74218934
Carlos Tiago <ontiago@gmail.com>
2024-08-19 17:33:27 -0300

    Cross Cutting Concerns

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,21 @@ import 'package:dio/dio.dart';
 import 'package:http/http.dart';
 
 main() async {
-  final logger = ConsoleLoggerAdapter();
-  final repo =
-      UserApiRepository(httpClient: makeHttpClientGet(), logger: logger);
+  final repo = UserApiRepository(httpClient: makeHttpClientGet());
   await repo.loadCurrentUser();
 }
 
-HttpGetClient makeHttpClientGet() => DioAdapter();
+HttpGetClient makeHttpClientGet() => DioAdapter(logger: ConsoleLoggerAdapter());
 
 final class UserApiRepository {
   final HttpGetClient httpClient;
-  final Logger logger;
 
   const UserApiRepository({
     required this.httpClient,
-    required this.logger,
   });
 
   Future<void> loadCurrentUser() async {
-    final json = await httpClient.get(url: "http://localhost:4000/users");
-    logger.log(key: 'loadCurrentUser', value: json);
+    await httpClient.get(url: "http://localhost:4000/users");
   }
 }
 
@@ -46,20 +41,36 @@ abstract interface class HttpGetClient {
 
 final class HttpAdapter implements HttpGetClient {
   final client = Client();
+  final Logger logger;
+
+  HttpAdapter({
+    required this.logger,
+  });
 
   @override
   Future<dynamic> get({required String url}) async {
     final response = await client.get(Uri.parse(url));
-    return jsonDecode(response.body);
+    final json = jsonDecode(response.body);
+    await logger.log(key: 'http_request', value: json);
+
+    return json;
   }
 }
 
 final class DioAdapter implements HttpGetClient {
   final client = Dio();
+  final Logger logger;
+
+  DioAdapter({
+    required this.logger,
+  });
 
   @override
   Future<dynamic> get({required String url}) async {
     final response = await client.get(url);
-    return response.data;
+    final json = response.data;
+    await logger.log(key: 'http_request', value: json);
+
+    return json;
   }
 }
